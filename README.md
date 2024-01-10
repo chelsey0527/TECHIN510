a194bfc1e86ff8b4845b06841dcb383b543cd8e7
Chelsey Lee <chelsey0527@gmail.com>
2024-01-09 16:55:09 -0800

    intial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Chelsey's personal website
+
+## Getting Started
+
+```
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.header("Hi, I am Chelsey")
+    st.subheader("Education")
+    st.write("University of Washington - Technology Innovation")
+    st.subheader("Work Experience")
+    st.write("Techinical Support Assistant @ GIX")
+    st.subheader("Hobbies")
+    st.write("Sleep")
+    st.subheader("Interesting project")
+    st.write("none")
+
+with col2:
+   st.image('chelsey.JPG', caption='Me')
+
