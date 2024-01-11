7f0b6707f8dcddfd18596b5ef89877e6e51358ce
Chelsey Lee <chelsey0527@gmail.com>
2024-01-11 12:48:36 -0800

    add more features

--- app.py
+++ app.py
@@ -1,18 +1,39 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
+
+chart_data = pd.DataFrame(np.random.randn(20, 4), columns=["col1", "Angry", "Sad", "Happy"])
+
+st.balloons()
+st.header("Hi, I am Chelsey")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.header("Hi, I am Chelsey")
     st.subheader("Education")
     st.write("University of Washington - Technology Innovation")
     st.subheader("Work Experience")
-    st.write("Techinical Support Assistant @ GIX")
+    st.write("Technical Support Specialist @ GIX")
+    st.write("Software Engineer @ TCFC")
+    st.write("Technical Assistant Intern @ Microsoft Corporation")
     st.subheader("Hobbies")
     st.write("Sleep")
     st.subheader("Interesting project")
     st.write("none")
 
 with col2:
-   st.image('chelsey.JPG', caption='Me')
+    st.image('chelsey.jpg', caption='Me')
+    st.link_button("LinkedIn", "https://www.linkedin.com/in/chinshanlee/", help=None, type="secondary", disabled=False, use_container_width=False)
+
+st.divider()
+
+st.header("My Favorite Song")
+st.video("https://youtu.be/wA3mVqo-jvg?si=6fWBvft5O7-k2TcY", format="video/mp4", start_time=0)
+
+st.divider()
+
+st.header("My Mental Health Status")
+st.line_chart(
+   chart_data, x="col1", y=["Angry", "Sad", "Happy"], color=["#E07A5F", "#A9BCD0", "#F2CC8F"]
+)
+
